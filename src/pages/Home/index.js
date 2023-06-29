cd3fdc47780620403e2b419d8ec6b445e3a6f971
sian <sian.m.stone@gmail.com>
2023-06-29 13:02:56 +0100

    adds donate-dev img to Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,32 @@
 import { Page } from './styles';
 
+import devImg from 'assets/donate-dev.png';
+
+import { Text, Layout, Image } from 'components';
+import { Header } from './styles';
+
 const Home = () => {
   return (
     <Page>
-      <h1>hi, i'm <span>siân</span>.</h1>
+      <Header>
+        <Text.Heading 
+          size={1}
+          textAlign='center'
+        >
+          hi, i'm <span>siân</span>.
+        </Text.Heading>
+      </Header>
+
+      <Layout.Row>
+        <Layout.Col size={6}>
+          <Image src={devImg} maxWidth={'100%'}/>
+        </Layout.Col>
+
+        <Layout.Col size={6}>
+
+        </Layout.Col>
+      </Layout.Row>
+
     </Page>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,16 +2,28 @@ import styled from 'styled-components';
 
 export const Page = styled.div(props => {
   const { theme } = props;
+  const { margin } = theme;
 
   return {
-    height: `100vh`,
-    width: `100vw`,
+    margin: theme.margin * 2,
+    height: `calc(100vh - ${theme.margin * 4}px)`,
+    width: `calc(100vw - ${theme.margin * 4}px)`,
     position: 'relative',
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
 
     'span': {
       color: theme.colors.main
     }
+  }
+})
+
+export const Header = styled.div(props => {
+  return {
+    textAlign: 'center',
+    width: '100%',
+    position: 'fixed',
+    top: props.theme.margin * 2
   }
 })
